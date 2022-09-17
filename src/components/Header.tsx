9d7e54097df9bd52c4a7dca69d87f39731546e2b
Igor Pedrosa <ilap1036@gmail.com>
2022-09-16 21:20:17 -0300

    feat: header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,21 @@
+import searchIcon from "~/assets/searchIcon.svg";
+
+import styles from "./header.module.scss";
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.content}>
+        <img
+          className={styles.pokeLogo}
+          alt="pokemon logo"
+          src="https://logosmarcas.net/wp-content/uploads/2020/05/Pokemon-Logo.png"
+        />
+        <div className={styles.searchField}>
+          <input type="text" placeholder="Pesquisar" />
+          <img className={styles.searchImg} alt="search" src={searchIcon} />
+        </div>
+      </div>
+    </header>
+  );
+}
